f54e3f7c85b5f15274ede7d08763043e99b6b96a
Devin Thomas <dwt@cs.unh.edu>
2026-03-05 11:40:21 -0500

    experiments

---
+++ bsbs_slu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=bsbs   # Job name
+#SBATCH --mail-type=NONE            # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=dwt@cs.unh.edu   # Where to send mail	
+#SBATCH --ntasks=1                  # Run a single task
+#SBATCH --array=1-50                 # Array range
+#SBATCH --time=0:30:00
+#SBATCH --mem=60G
+#SBATCH --no-kill
+#SBATCH -p compute
+cd /home/aifs2/devin/Documents/search
+eval "$(head -${SLURM_ARRAY_TASK_ID} bsbs_test.sh | tail -1)"
---
+++ experiments.py
@@ -0,0 +1,26 @@
+command =  "./tiles/15md_solver {algorithm}  -wt {subopt} -width {width} < /home/aifs2/group/data/tiles_instances/korf/4/4/{korfnum} > ./results/{outfile}" 
+widths = [4**i for i in range(2, 6)]  # [16, 64, 256, 1024]
+weights = [1.03, 1.05, 1.1, 1.3, 1.5, 2, 3, 5, 10]
+
+korf_nums = list(range(1, 101))
+
+algorithms = {
+    "astar": {"width": False, "weight": False},
+    "wastar": {"width": False, "weight": True},
+     "bead": {"width": True, "weight": False},
+     "bsbs": {"width": True, "weight": True}
+}
+
+
+for algorithm in algorithms:
+    alg_widths = [-1]
+    if algorithms[algorithm]["width"]:
+        alg_widths = widths
+    alg_weights = [-1]
+    if algorithms[algorithm]["weight"]:
+        alg_weights = weights
+    for width in alg_widths:
+        for weight in alg_weights:
+            for inst in korf_nums:
+                ofname = "_".join([algorithm, str(weight), str(width), str(inst)])  
+                print(command.format(algorithm = algorithm, subopt = weight, width = width, korfnum = inst, outfile =ofname))
