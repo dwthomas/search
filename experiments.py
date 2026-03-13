fadaff3efc697e1fba5d353d9f6f42043026e76b
Devin Wild Thomas <dwt@cs.unh.edu>
2026-03-13 16:03:31 -0400

    exp

--- experiments.py
+++ experiments.py
@@ -1,23 +1,24 @@
-command =  "/home/aifs2/dwr29/search/tiles/15md_solver {algorithm} -aspect {aspect} -cost {cost} -wt {subopt} -width {width} < /home/aifs2/group/data/tiles_instances/korf/4/4/{korfnum} > /home/aifs2/dwr29/search/results/{outfile}" 
+command =  "/home/aifs2/dwr29/search/tiles/15md_solver {algorithm} -cost {cost} -wt {subopt} -width {width} < /home/aifs2/group/data/tiles_instances/korf/4/4/{korfnum} > /home/aifs2/dwr29/search/results/{outfile}" 
 widths = [4**i for i in range(2, 6)]  # [16, 64, 256, 1024]
 weights = [1.03, 1.05, 1.07, 1.1, 1.2, 1.3, 1.4, 1.5, 1.7,  2, 3, 5, 10]
+weights = [2.25, 2.8]
 korf_nums = list(range(1, 101))
 
 costs = ["unit", "heavy", "inverse"]
 
-aspects = [1, 100]
+# aspects = [1, 100]
 
 algorithms = {
-    # "astar": {"width": False, "weight": False, "aspect": False},
-    "rectangle": {"width": False, "weight": False, "aspect": True},
-    "boundedrectangle": {"width": False, "weight": True, "aspect": True},
-    # "wastar": {"width": False, "weight": True, "aspect": False},
-    # "bead": {"width": True, "weight": False, "aspect": False},
-    # "bsbs": {"width": True, "weight": True, "aspect": False},
-    # "rrd": {"width": False, "weight": True, "aspect": False},
-    # "rrdnofocal": {"width": False, "weight": True, "aspect": False},
-    # "rrdnoopen": {"width": False, "weight": True, "aspect": False},
-    # "ees": {"width": False, "weight": True, "aspect": False},
+    "astar": {"width": False, "weight": False, "aspect": False},
+    # "rectangle": {"width": False, "weight": False, "aspect": True},
+    # "boundedrectangle": {"width": False, "weight": True, "aspect": True},
+    "wastar": {"width": False, "weight": True, "aspect": False},
+    "bead": {"width": True, "weight": False, "aspect": False},
+    "bsbs": {"width": True, "weight": True, "aspect": False},
+    "rrd": {"width": False, "weight": True, "aspect": False},
+    "rrdnofocal": {"width": False, "weight": True, "aspect": False},
+    "rrdnoopen": {"width": False, "weight": True, "aspect": False},
+    "ees": {"width": False, "weight": True, "aspect": False},
 }
 
 for cost in costs:
@@ -29,12 +30,12 @@ for cost in costs:
         if algorithms[algorithm]["weight"]:
             alg_weights = weights
         alg_aspects = [-1]
-        if algorithms[algorithm]["aspect"]:
-            alg_aspects = aspects
+        # if algorithms[algorithm]["aspect"]:
+        #     alg_aspects = aspects
 
         for width in alg_widths:
             for weight in alg_weights:
                 for aspect in alg_aspects:
                     for inst in korf_nums:
-                        ofname = "_".join([algorithm, cost, str(weight), str(width), str(aspect), str(inst)])  
-                        print(command.format(algorithm = algorithm, cost = cost, subopt = weight, width = width, aspect = aspect, korfnum = inst, outfile =ofname))
+                        ofname = "_".join([algorithm, cost, str(weight), str(width), str(inst)])  
+                        print(command.format(algorithm = algorithm, cost = cost, subopt = weight, width = width, korfnum = inst, outfile =ofname))
