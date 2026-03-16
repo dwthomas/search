d6b3b869ff49f30d23ec90a97811e944ffc05eea
Devin Wild Thomas <dwt@cs.unh.edu>
2026-03-16 17:30:23 -0400

    pancake slurm script

---
+++ bsbs_slupancake.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=bsbs   # Job name
+#SBATCH --mail-type=NONE            # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=dwt@cs.unh.edu   # Where to send mail	
+#SBATCH --ntasks=1                  # Run a single task
+#SBATCH --array=1-33900                 # Array range
+#SBATCH --time=0:5:00
+#SBATCH --mem=60G
+#SBATCH --no-kill
+#SBATCH -p compute
+eval "$(head -${SLURM_ARRAY_TASK_ID} pancakeexp.sh | tail -1)"
