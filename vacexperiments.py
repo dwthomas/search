3826202f0ce7f28a9b72148c510dfb76a7612b22
Devin Wild Thomas <dwt@cs.unh.edu>
2026-03-21 16:02:49 -0400

    vacexp

---
+++ vacexperiments.py
@@ -0,0 +1,55 @@
+import numpy as np
+from glob import glob
+
+instances = glob("/home/aifs2/group/data/vacuum_instances/uniform/200/200/*/*/*")
+
+command =  "cat {instance} | /home/aifs2/dwr29/search/vacuum/convert_vacex.py | /home/aifs2/dwr29/search/vacuum/vacuum_solver {algorithm} -cost {cost} -wt {subopt} -width {width} > /home/aifs2/dwr29/search/pancake_results/{outfile}" 
+
+
+widths = [4**i for i in range(2, 6)]  # [16, 64, 256, 1024]
+weights = np.geomspace(1.03, 5, num=6)
+# weights = [1.03, 1.05, 1.07, 1.1, 1.2, 1.3, 1.4, 1.5, 1.7,  2, 2.25, 2.8, 3, 5, 10]
+# weights = [2.25, 2.8]
+widths = [4**i for i in range(2, 8)]  # [16, 64, 256, 1024]
+# widths = [4**i for i in range(6, 8)]  # [16, 64, 256, 1024]
+# weights = [1.03, 1.05, 1.07, 1.1, 1.2, 1.3, 1.4, 1.5, 1.7,  2, 2.25, 2.8, 3, 5, 10]
+# weights = [1.03, 1.07, 1.2, 1.4, 1.7,  2, 3]
+
+costs = ["unit", "heavy", "inverse"]
+
+# aspects = [1, 100]
+
+algorithms = {
+    "astar": {"width": False, "weight": False, "aspect": False},
+    # "rectangle": {"width": False, "weight": False, "aspect": True},
+    # "boundedrectangle": {"width": False, "weight": True, "aspect": True},
+    # "wastar": {"width": False, "weight": True, "aspect": False},
+    "wastar -dropdups": {"width": False, "weight": True, "aspect": False},
+    "bead": {"width": True, "weight": False, "aspect": False},
+    "bsbs": {"width": True, "weight": True, "aspect": False},
+    # "bsbsflayer": {"width": True, "weight": True, "aspect": False},
+    # "bsbsfill": {"width": True, "weight": True, "aspect": False},
+    "rrd": {"width": False, "weight": True, "aspect": False},
+    # "rrdnofocal": {"width": False, "weight": True, "aspect": False},
+    # "rrdnoopen": {"width": False, "weight": True, "aspect": False},
+    # "ees": {"width": False, "weight": True, "aspect": False},
+}
+
+for cost in costs:
+    for algorithm in algorithms:
+        alg_widths = [-1]
+        if algorithms[algorithm]["width"]:
+            alg_widths = widths
+        alg_weights = [-1]
+        if algorithms[algorithm]["weight"]:
+            alg_weights = weights
+        alg_aspects = [-1]
+        # if algorithms[algorithm]["aspect"]:
+        #     alg_aspects = aspects
+
+        for width in alg_widths:
+            for weight in alg_weights:
+                for aspect in alg_aspects:
+                    for inst in instances:
+                        ofname = "_".join([algorithm.replace(" ", ""), cost, str(weight), str(width), str("-".join(inst.split("/")[-6:]))])  
+                        print(command.format(algorithm = algorithm, cost = cost, subopt = weight, width = width, korfnum = inst, outfile =ofname))
