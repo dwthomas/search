820f6ba5848ed40c010c3e79cb23d62a00d7757b
Devin Wild Thomas <dwt@cs.unh.edu>
2026-04-14 11:41:10 -0400

    pancake exp

---
+++ pkjobs/slurm_submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=pkjobs
+#SBATCH --mail-type=NONE
+#SBATCH --mail-user=dwt@cs.unh.edu
+#SBATCH --ntasks=1
+#SBATCH --array=1-128
+#SBATCH --time=15:5:00
+#SBATCH --mem=60G
+#SBATCH --no-kill
+#SBATCH -p compute
+eval "job_${SLURM_ARRAY_TASK_ID}.sh"
