680295c933e216309c7f1e061b9a36ba7f346c38
Devin Wild Thomas <dwt@cs.unh.edu>
2026-04-14 11:57:00 -0400

    round 3

--- pkjobs/slurm_submit.sh
+++ pkjobs/slurm_submit.sh
@@ -8,4 +8,4 @@
 #SBATCH --mem=60G
 #SBATCH --no-kill
 #SBATCH -p compute
-eval "/home/aifs2/dwr29/search/{jobs_dir}/job_${SLURM_ARRAY_TASK_ID}.sh"
+eval "/home/aifs2/dwr29/search/pkjobs/job_${SLURM_ARRAY_TASK_ID}.sh"
