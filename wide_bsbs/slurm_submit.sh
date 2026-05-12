e81457ea6313fef07cff6324b5fd7ed973806f25
Devin Wild Thomas <dwt@cs.unh.edu>
2026-05-12 11:17:03 -0400

    exp - wide

---
+++ wide_bsbs/slurm_submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=wide_bsbs
+#SBATCH --mail-type=NONE
+#SBATCH --mail-user=dwt@cs.unh.edu
+#SBATCH --ntasks=1
+#SBATCH --array=1-512
+#SBATCH --time=15:5:00
+#SBATCH --mem=60G
+#SBATCH --no-kill
+#SBATCH -p compute
+eval "/home/aifs2/dwr29/search/wide_bsbs/job_${SLURM_ARRAY_TASK_ID}.sh"
